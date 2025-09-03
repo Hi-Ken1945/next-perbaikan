c43ce7d210b3ca210188e4975cf004114dd9ffff
BalyaaGz <adikmuridngendok@gmail.com>
2025-09-03 11:39:33 +0700

    Update page.tsx

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -8,6 +8,17 @@ export default function Projects() {
           <p>Program tentang Pembelian Buku Tanpa ada masalah</p>
         </article>
         <article className="project-item">
+  <h3>Github</h3>
+  <a
+    href="https://github.com/Hi-Ken1945/next-perbaikan"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-blue-500 underline hover:text-blue-700"
+  >
+    Lihat Sendiri
+  </a>
+</article>
+        <article className="project-item">
           <h3>Ibis Paint</h3>
           <p>
             Belajar Desain dan memahami tools-tools yang ada di Ibis Paint
